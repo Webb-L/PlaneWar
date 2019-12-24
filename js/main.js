6e1f531e52f41dd99f62366818e9a26ee3cfeb22
Your Name <you@example.com>
2019-12-24 12:04:45 +0800

    差结算数据

--- js/main.js
+++ js/main.js
@@ -24,6 +24,11 @@ Game.prototype.init = function () {
   this.createTime = 1000;
   // 敌方飞机移动时间（毫秒）
   this.movingTime = 800;
+  //
+  this.TimingInterval;
+  this.OurEnemyPlaneCollidedTime;
+  this.BulletEnemyCollisionTime;
+  this.randomPlaneTime;
 
   // 游戏区域宽度
   this.width = this.gameRegion.clientWidth;
@@ -49,7 +54,7 @@ Game.prototype.init = function () {
 // 计时器
 Game.prototype.timing = function () {
   let second = 1;
-  setInterval(() => {
+  this.TimingInterval = setInterval(() => {
     this.time.innerText = second++;
   }, 1000)
 };
@@ -58,20 +63,25 @@ Game.prototype.timing = function () {
 Game.prototype.randomPlane = function () {
   let that = this;
   // 创建飞机
-  setInterval(() => {
+  this.randomPlaneTime = setInterval(() => {
     let img = new Image();
     let type = Math.floor(Math.random() * (4 - 1) + 1);
     img.src = '../img/e' + type + '.png';
     img.setAttribute('data-type', type);
     img.setAttribute('class', 'enemy');
     img.style.position = 'absolute';
+    img.style.border = "1px solid red";
     img.style.top = "-80px";
     if (img.complete) {
       img.style.left = Math.floor(Math.random() * (that.width - img.width)) + 'px';
+      img.style.width = img.width + 'px';
+      img.style.height = img.height + 'px';
     } else {
       img.onload = function () {
         img.onload = null;
         img.style.left = Math.floor(Math.random() * (that.width - img.width)) + 'px';
+        img.style.width = img.width + 'px';
+        img.style.height = img.height + 'px';
       };
     }
     // 将飞机添加到游戏区
@@ -88,8 +98,23 @@ Game.prototype.airplaneMovement = function (img) {
     y += 5;
     img.style.top = y + 'px';
     if (y >= this.height + (h * 2)) {
-      clearInterval(time);
       img.remove();
+      clearInterval(time);
+      console.log(img.getAttribute('data-type'));
+      switch (img.getAttribute('data-type')) {
+        case '1':
+          this.fraction.innerText = parseInt(this.fraction.innerText) - (parseInt(img.getAttribute('data-type')) * 10) * 5;
+          this.gameOver();
+          break;
+        case '2':
+          this.fraction.innerText = parseInt(this.fraction.innerText) - (parseInt(img.getAttribute('data-type')) * 10) * 5;
+          this.gameOver();
+          break;
+        case '3':
+          this.fraction.innerText = parseInt(this.fraction.innerText) - (parseInt(img.getAttribute('data-type')) * 10) * 5;
+          this.gameOver();
+          break;
+      }
     }
   }, 1000 / 60)
 };
@@ -104,6 +129,7 @@ Game.prototype.ourPlane = function () {
   div.style.zIndex = '10';
   div.style.backgroundImage = "url(" + img.src + ")";
   div.style.cursor = "cell";
+  div.style.border = "1px solid blue";
   div.setAttribute('class', 'ourPlane');
 
   if (img.complete) {
@@ -137,19 +163,57 @@ Game.prototype.ourPlane = function () {
 
 // 碰撞检测
 Game.prototype.collision = function () {
-  setInterval(() => {
-    let div = document.querySelector('.game .ourPlane');
+  // 我方飞机和敌方飞机碰撞
+  this.OurEnemyPlaneCollidedTime = setInterval(() => {
     let img = document.querySelectorAll('.enemy');
-    img.forEach((e, i) => {
-      if (div.offsetLeft < e.x + e.width &&
-        div.offsetLeft + div.offsetWidth > e.x &&
-        div.offsetTop < e.y + e.height &&
-        div.offsetHeight + div.offsetTop > e.y) {
+    img.forEach(e => {
+      if (this.me.x < parseInt(e.style.left) + e.width &&
+        this.me.x + this.me.width > parseInt(e.style.left) &&
+        this.me.y < parseInt(e.style.top) + e.height &&
+        this.me.height + this.me.y > parseInt(e.style.top)) {
         e.remove();
-        console.log("碰撞减去判断", i);
+        e.setAttribute('data-type', '0');
       }
     })
-  }, 50)
+  }, 50);
+
+  // 子弹和敌方飞机碰撞
+  this.BulletEnemyCollisionTime = setInterval(() => {
+    let img = document.querySelectorAll('.enemy');
+    let bullet = document.querySelectorAll('.bullet');
+    img.forEach(e => {
+      bullet.forEach(eve => {
+        if (parseInt(e.style.left) < parseInt(eve.style.left) + eve.width &&
+          parseInt(e.style.left) + e.width > parseInt(eve.style.left) &&
+          parseInt(e.style.top) < parseInt(eve.style.top) + eve.height &&
+          e.height + parseInt(e.style.top) > parseInt(eve.style.top)) {
+          e.src = '../img/boom.gif';
+          switch (e.getAttribute('data-type')) {
+            case '1':
+              this.fraction.innerText = parseInt(this.fraction.innerText) + parseInt(e.getAttribute('data-type')) * 10;
+              eve.remove();
+              break;
+            case '2':
+              this.fraction.innerText = parseInt(this.fraction.innerText) + parseInt(e.getAttribute('data-type')) * 10;
+              eve.remove();
+              break;
+            case '3':
+              this.fraction.innerText = parseInt(this.fraction.innerText) + parseInt(e.getAttribute('data-type')) * 10;
+              eve.remove();
+              break;
+          }
+          e.setAttribute('data-type', '0');
+          if (this.fraction.innerText >= 500) this.fraction.innerText = 0;
+          let num = 1;
+          let disappear = setInterval(() => {
+            num = num - 0.05;
+            e.style.opacity = num;
+            if (num <= 0) clearInterval(disappear);
+          }, 1000 / 60);
+        }
+      });
+    });
+  }, 50);
 };
 
 // 子弹发射
@@ -164,6 +228,8 @@ Game.prototype.bulletLaunch = function (div) {
     bullet.style.top = y + that.me.height + 'px';
     bullet.style.left = (x + that.me.width / 2) + 'px';
     bullet.style.zIndex = 1;
+    bullet.style.border = "1px solid yellow";
+    bullet.setAttribute('class', 'bullet');
     that.gameRegion.appendChild(bullet);
     that.bulletMovement(bullet);
   };
@@ -179,4 +245,37 @@ Game.prototype.bulletMovement = function (bullet) {
       bullet.remove();
     }
   }, 1000 / 60)
+};
+// 游戏结束
+Game.prototype.gameOver = function () {
+  if (this.fraction.innerText <= 0) {
+    document.querySelector('.popup').style.display = 'flex';
+    document.querySelector('.popup .endTime').innerText = this.time.innerText;
+
+    if (this.time.innerText <= 30) {
+      document.querySelector('.popup .rank').innerText = "倔强青铜";
+    } else if (30 < this.time.innerText && this.time.innerText <= 60) {
+      document.querySelector('.popup .rank').innerText = "秩序白银";
+    } else if (60 < this.time.innerText && this.time.innerText <= 120) {
+      document.querySelector('.popup .rank').innerText = "荣耀黄金";
+    } else if (120 < this.time.innerText && this.time.innerText <= 240) {
+      document.querySelector('.popup .rank').innerText = "尊贵铂金";
+    } else if (240 < this.time.innerText && this.time.innerText <= 480) {
+      document.querySelector('.popup .rank').innerText = "永恒钻石";
+    } else if (this.time.innerText > 480) {
+      document.querySelector('.popup .rank').innerText = "最强王者";
+    }
+
+    if (document.querySelector('.ourPlane')) document.querySelector('.ourPlane').remove();
+    document.querySelectorAll('.bullet').forEach(e => {
+      e.remove();
+    });
+    document.querySelectorAll('.enemy').forEach(e => {
+      e.remove();
+    });
+    clearInterval(this.TimingInterval);
+    clearInterval(this.OurEnemyPlaneCollidedTime);
+    clearInterval(this.BulletEnemyCollisionTime);
+    clearInterval(this.randomPlaneTime);
+  }
 };
